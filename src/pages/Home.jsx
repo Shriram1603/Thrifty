4401de7ac9296ab843caf3207dc001b72cebf481
Shriram1603 <114002708+Shriram1603@users.noreply.github.com>
2023-08-01 19:12:30 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Slider from '../components/Slider'
+import Topbar from '../components/Topbar/Topbar'
+
+const Home = () => {
+  return (
+    <div>
+        <Topbar/>
+        <Slider/>
+    </div>
+  )
+}
+
+export default Home
